e9813fdc1b791164f4a3504bda915ad44e5fceb0
Jean-Christophe Paradis <jparadis@c1r2p6.42quebec.com>
2022-03-03 12:13:13 -0500

    ft_print_alphabet fix

--- ex01/ft_print_alphabet.c
+++ ex01/ft_print_alphabet.c
@@ -6,12 +6,12 @@
 /*   By: jparadis <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 13:32:43 by jparadis          #+#    #+#             */
-/*   Updated: 2022/03/01 13:26:14 by jparadis         ###   ########.fr       */
+/*   Updated: 2022/03/03 12:11:31 by jparadis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 void	ft_print_alphabet(void)
 {
-	write(1, "abcdefghijklmnopqrstuvwyz", 26);
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
 }
